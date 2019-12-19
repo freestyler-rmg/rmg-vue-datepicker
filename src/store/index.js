294810511db8ff756dcba24d00b47498232798b3
Roy Margasa <roy-aleph@Aleph-Roy-Margasa.local>
2019-12-19 16:09:01 +0700

    preparation for moving local state to vuex

--- src/store/index.js
+++ src/store/index.js
@@ -3,4 +3,12 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
-export const store = new Vuex.Store();
+export const store = new Vuex.Store({
+  state: {
+    today: new Date(),
+    months: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Des"],
+    daysName: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+    startDay: 0,
+    dates: []
+  },
+});
